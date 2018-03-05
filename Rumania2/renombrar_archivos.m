70ca946437f4a57128fd71c1d974f7ad00295b83
davizinho <dalvar_5@yahoo.es>
2018-03-05 16:00:40 +0100

    finished app

--- Rumania2/renombrar_archivos.m
+++ Rumania2/renombrar_archivos.m
@@ -1,18 +1,19 @@
 % Pone a todos los archivos un nombre con un numero creciente
 
+cd I/
 %% Lista todos los archivos con la extension ".jpg"
-filesBAG = dir('*.jpg');
+files = dir('*.png');
 
 %%  Renombra los archivos en el directorio
-for num_file = 1:length(filesBAG) % Ciclo para cada archivo
-    [~, filename] = fileparts(filesBAG(num_file).name); % Obtiene el nombre sin la extension
+for num_file = 1:length(files) % Ciclo para cada archivo
+    [~, filename] = fileparts(files(num_file).name); % Obtiene el nombre sin la extension
     try
         if ~strcmp(strtok(filename,'_'),'HD')
             if num_file == 1
                 disp(' ** Renombrando los archivos');
             end
-            movefile(filesBAG(num_file).name, sprintf('%d.jpg', num_file)); % Renombra los archivos
-            if num_file == length(filesBAG)
+            movefile(files(num_file).name, sprintf('%d.jpg', num_file)); % Renombra los archivos
+            if num_file == length(files)
                 fprintf(' *** Un total de: %d archivos han sido renombrados \n', num_file);
             end
         else
@@ -25,3 +26,5 @@ for num_file = 1:length(filesBAG) % Ciclo para cada archivo
         break;
     end
 end
+
+cd ..
